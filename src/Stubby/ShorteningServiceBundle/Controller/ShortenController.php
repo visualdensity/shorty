503df6eb33aa91e0171adc12a69d0b4eeb1e6349
Wee Keat Chin <weekeat@visualdensity.com>
2013-05-21 06:49:36 +0200

    Renamed files, extract-to-repository and a simple view

---
+++ src/Stubby/ShorteningServiceBundle/Controller/ShortenController.php
@@ -0,0 +1,51 @@
+<?php
+namespace Stubby\ShorteningServiceBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\AcceptHeader;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Stubby\ShorteningServiceBundle\Entity\Url;
+use Stubby\ShorteningServiceBundle\Entity\Click;
+
+/**
+ * @Route("/api/shorten")
+ */
+class ShortenController extends Controller
+{
+    /**
+     * Lists all Urls entities.
+     *
+     * @Route("/", name="shorten")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return $this->redirect($this->generateUrl('shorten_list'));
+    }
+
+	/**
+	 * Lists all Urls entities.
+	 * @Route("/list", name="shorten_list")
+     * @Method("GET")
+     * @Template()
+	 */
+	public function listAction(Request $request)
+    {	
+		$em = $this->getDoctrine()->getManager();
+		
+		$itemsCount = count($em->getRepository('StubbyShorteningServiceBundle:Url')->findAll());
+        $query = $em->createQuery( 'select u from StubbyShorteningServiceBundle:Url u' );
+        $entities = $query->getResult();
+
+        return array(
+            'entities'  => $entities,
+            'base_href' => $this->container->getParameter('base_href'),
+        );
+    }
+
+}
